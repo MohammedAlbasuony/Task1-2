8d2faaffa65693fb9d72b28cbe88dc17a5a06900
Mohammed Albasuony <Mido.beso.hosny@gmail.com>
2024-07-28 18:34:03 +0300

    Add project files.

---
+++ Task 1-2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Student name :  ");
+            string StdName = Console.ReadLine();
+
+            Console.Write("Enter Student ID :  ");
+            int StdID = int.Parse(Console.ReadLine());
+            
+            Console.Write("Enter Student Department :  ");
+            string StdDept = Console.ReadLine();
+            
+            Console.Write("Enter Student Age :  ");
+            int StdAge = int.Parse(Console.ReadLine());
+            
+            Console.Write($"Your name is : {StdName} \n  Your Age is : {StdAge} \n  Your ID is : {StdID}  \n Your Department is : {StdDept} ");
+        }
+    }
+}
